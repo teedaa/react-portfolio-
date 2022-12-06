9efe62e98bbe8f48ed16381c47955bac549a977a
teedaa <pengtedachengllhs@gmail.com>
2022-12-05 23:51:22 -0800

    Fix image styling

--- src/components/about.js
+++ src/components/about.js
@@ -6,12 +6,12 @@ export default function About() {
         <div className="container-fluid">
             <div className="container shadow-lg" id="about">
                 <div className="row mb-5 rounded about-me-box">
-                    <div className="col-sm-4 p-4 m-auto aboutText">
+                    <div className="col-sm-4 p-4 m-auto about-me-box ">
                         <h2 className="text-center">About Me</h2>
-                        <p>My name is Pengteda, and I currently reside in Lake County, California. My previous academic background includes a Bachelor of Arts in Spanish from Sonoma State University, and a minor in Astronomy. I am currently a student at the UC Berkeley Full Stack Coding Bootcamp and I am set to earn my certificate in mid-December.
+                        <p >My name is Pengteda, and I currently reside in Lake County, California. My previous academic background includes a Bachelor of Arts in Spanish from Sonoma State University, and a minor in Astronomy. I am currently a student at the UC Berkeley Full Stack Coding Bootcamp and I am set to earn my certificate in mid-December.
                         </p>
                     </div>
-                    <div className="col-sm-4 m-auto">
+                    <div className="col-sm-4 m-auto shadow-lg me">
                         <img src={require('../img/me.png')} alt='a portrait of me' className="about"/>
                     </div>
                 </div>
